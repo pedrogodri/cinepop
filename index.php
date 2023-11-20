e3f66f4f9be4fe3e8babad0b8a7fc73f640befd2
Pedro Godri <pedrogodri@gmail.com>
2023-11-20 16:51:51 -0300

    Iniciando projeto final

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CinePop</title>
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
