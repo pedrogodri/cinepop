095d79b42db6131b4f8fb316016b656bb0bae163
Pedro Godri <pedrogodri@gmail.com>
2023-11-21 12:04:42 -0300

    tela login

--- auth.php
+++ auth.php
@@ -2,8 +2,55 @@
     require_once("templates/header.php");
 ?>
 
+<link rel="stylesheet" href="<?= $BASE_URL ?>css/auth.css">
+
 <div id="main-container" class="container-fluid">
-    <h1>Autenticação</h1>
+    <div class="com-md-12">
+        <div class="row" id="auth-row">
+            <div class="col-md-4" id="login-container">
+                <h2 class="title-form">Entrar</h2>
+                <form action="" method="POST" class="form-register-login">
+                <input type="hidden" name="type" value="login">
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digite seu senha">
+                    </div>
+                    <input type="submit" class="btn card-btn mt-4" value="Entrar">
+                </form>
+            </div>
+            <div class="col-md-4" id="register-container">
+                <h2 class="title-form">Criar conta</h2>
+                <form action="" method="POST" class="form-register-login">
+                    <input type="hidden" name="type" value="register">
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Digite seu nome">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Sobrenome:</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Digite seu sobrenome">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digite seu senha">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmpassword">Confirmação de senha:</label>
+                        <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirme sua senha">
+                    </div>
+                    <input type="submit" class="btn card-btn mt-4" value="Cadastrar">
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php 
