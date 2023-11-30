8d01581fa67ce0539560867e39050d1acddbdd45
PedroGodri <pedro.g2006@aluno.ifsc.edu.br>
2023-11-30 12:15:39 -0300

    Feat: img

--- models/User.php
+++ models/User.php
@@ -21,6 +21,10 @@
         public function generatePassword($password) {
             return password_hash($password, PASSWORD_DEFAULT);
         }
+
+        public function imageGenerateName() {
+            return bin2hex(random_bytes(60)) . ".jpg";
+        }
     }
 
 ?>
--- user_process.php
+++ user_process.php
@@ -27,6 +27,28 @@
         $userData->email = $email;
         $userData->bio = $bio;
 
+        if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+            $image = $_FILES["image"];
+            $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+            $jpgArray = ["image/jpeg", "image/jpg"];
+
+            if(in_array($image["type"], $imageTypes)) {
+
+                if(in_array($image, $jpgArray)) {
+                    $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+                } else {
+                    $imageFile = imagecreatefrompng($image["tmp_name"]);
+                }
+
+                $imageName = $user->imageGenerateName();
+                imagejpeg($imageFile, "./img/users/" . $imageName, 100);
+
+                $userData->image = $imageName;
+            } else {
+                $message->setMessage("Tipo inválido de imagem!", "alert-danger", "back");
+            }
+        }
+
         $userDAO->update($userData, true);
 
     } else if ($type == "changepassword") {
