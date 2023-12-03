0d08cc8c763c965d9f6e05d8cb7d89886b635c61
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-12-03 10:55:15 -0300

    Front-end de cadastro de filme

--- globals.php
+++ globals.php
@@ -1,4 +1,4 @@
 <?php 
     session_start();
-    $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . ":80" . dirname($_SERVER["REQUEST_URI"]."?") . "/";
+    $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . ":8080" . dirname($_SERVER["REQUEST_URI"]."?") . "/";
 ?>
---
+++ newmovie.php
@@ -0,0 +1,52 @@
+<?php 
+    require_once("templates/header.php");
+
+    require_once("models/User.php");
+    require_once("dao/UserDAO.php");
+
+    $userDAO = new UserDAO($conn, $BASE_URL);
+    $userData = $userDAO->verifyToken(true);
+?>
+
+<div id="main-container" class="container-fluid">
+    <div class="offset-md-4 col-md-4 new-movie-container">
+        <h1 class="page-title">Adicionar Filme</h1>
+        <p class="page-description">Adicione sua crítica e compartilhe com o mundo</p>
+        <form action="<?= $BASE_URL ?>movie_process.php" id="add-movie-form" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="title">Título:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Digite o título do seu filme">
+            </div>
+            <div class="form-group">
+                <label for="image">Imagem:</label>
+                <input type="file" class="form-control-file" id="image" name="image">
+            </div>
+            <div class="form-group">
+                <label for="category">Categoria:</label>
+                <select class="form-control" name="category" id="category">
+                    <option value="">Selcione</option>
+                    <option value="Ação">Ação</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Comédia">Comédia</option>
+                    <option value="Romance">Romance</option>
+                    <option value="Ficção">Ficção</option>
+                    <option value="Terror">Terror</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="trailer">Trailer:</label>
+                <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Insira o link do trailer">
+            </div>
+            <div class="form-group">
+                <label for="description">Descrição:</label>
+                <textarea class="form-control" name="description" id="description" rows="5" placeholder="Descreva o filme"></textarea></textarea></textarea>
+            </div>
+            <input type="submit" class="btn card-btn" value="Adicionar filme">
+        </form>
+    </div>
+</div>
+
+<?php 
+    require_once("templates/footer.php");
+?>
